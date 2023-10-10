1e6da70b9ec9b46b4dd9a012b4dabf28b9677228
koy23 <jekoy.carandang@gmail.com>
2023-10-10 13:28:40 +0800

    Removed project!

--- ext/jericho.js
+++ ext/jericho.js
@@ -101,46 +101,5 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
   
-  
-  const API_KEY = process.env.MY_API_KEY;
-  const submitIcon = document.querySelector("#SUBMITICON");
-  const inputElement = document.querySelector("input");
-  const imageSection = document.querySelector('.image-section')
-
-  const getImages = async () => {
-    const options = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: inputElement.value,
-        n: 2,
-        size: "1024x1024",
-      }),
-    };
-    try {
-      const response = await fetch(
-        "https://api.openai.com/v1/images/generations",
-        options
-      );
-      const data = await response.json(); 
-	  data?.data.forEach(imageResult => {
-		  const imageContainer = document.createElement ('div')
-		  imageContainer.classList.add('container-image')
-		  const imageElement = document.createElement('img')
-		  imageElement.setAttribute('src', imageResult.url)
-		  imageContainer.append (imageElement)
-		  imageSection.append(imageContainer)
-		  
-	  }
-	  )
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  submitIcon.addEventListener("click", getImages);
 });
  
